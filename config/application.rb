d4729094c4eca6f557b864dc1160919ebb70a8c3
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2021-07-27 19:59:08 -0300

    tabela de reservas prototipada

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module RailsApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.beginning_of_week = :monday
+    config.end_of_week = :friday
   end
 end
